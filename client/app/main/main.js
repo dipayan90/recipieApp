7f8d3cb70e93cda572009e12cf47cf377716e100
Chattopadyay <chattod@sea-chattod-m.ds.ad.adp.com>
2015-04-11 10:36:47 -0700

    First commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('recipieAppApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
