c67ed1fbbd7e7800ed6b37d19edbc6579dd25fc8
Dipayan <dipayan90@gmail.com>
2015-04-11 20:26:25 -0700

    Adding elastic search support

--- server/app.js
+++ server/app.js
@@ -27,6 +27,7 @@ var socketio = require('socket.io')(server, {
 require('./config/socketio')(socketio);
 require('./config/express')(app);
 require('./routes')(app);
+require('./components/uploads/load_recipies');
 
 // Start server
 server.listen(config.port, config.ip, function () {
--- server/routes.js
+++ server/routes.js
@@ -18,11 +18,6 @@ module.exports = function(app) {
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
 
-  app.route('/about')
-    .get(function(req,res){
-      res.sendfile(app.get('appPath') + '/app/main/aboutUs/aboutus.html');
-    });
-
   // All other routes should redirect to the index.html
   app.route('/*')
     .get(function(req, res) {
