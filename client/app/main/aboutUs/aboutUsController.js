98aa57936f9f3e8efde21d376461296a8de1cb8b
Dipayan <dipayan90@gmail.com>
2015-04-11 22:58:32 -0700

    Completed about us page.

--- client/app/main/aboutUs/aboutUsController.js
+++ client/app/main/aboutUs/aboutUsController.js
@@ -9,4 +9,15 @@
 angular.module('recipieAppApp')
   .controller('aboutCtrl', function ($scope, $http, socket) {
     $scope.aboutUsCategories = ['Our Idea','Our Team'];
+
+    $scope.showIdea = true;
+
+    $scope.showCategory = function(categoryClicked){
+      if(categoryClicked === $scope.aboutUsCategories[0]){
+        $scope.showIdea = true;
+      }else if(categoryClicked === $scope.aboutUsCategories[1]){
+        console.log("Hello");
+        $scope.showIdea = false;
+      }
+    }
   });
